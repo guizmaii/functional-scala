078c11306b17ecd2676bc2dd07af569a16851ee5
jules Ivanic <jules.ivanic@gmail.com>
2018-09-01 08:57:59 +0200

    Program algebra

--- src/main/scala/net/degoes/01-essentials/exercises.scala
+++ src/main/scala/net/degoes/01-essentials/exercises.scala
@@ -2,6 +2,25 @@
 
 package net.degoes.essentials
 
-object exercises {
-  
+object EffectsAsValues {
+
+  sealed trait Program[A] { self =>
+    final def map[B](f: A => B): Program[B] = flatMap(a => Return(f(a)))
+
+    final def flatMap[B](f: A => Program[B]): Program[B] =
+      self match {
+        case Return(a) => f(a)
+        case x         => Chain(x, f)
+      }
+  }
+  object Program {
+    final def point[A](a: => A): Program[A] = Return(a)
+  }
+  final case class Return[A](a: A)                                    extends Program[A]
+  final case class GetStrLn[A](next: String => Program[A])            extends Program[A] // next is named a continuation.
+  final case class PutStrLn[A](line: String, next: Program[A])        extends Program[A]
+  final case class Chain[A0, A](previous: A0, next: A0 => Program[A]) extends Program[A]
+
 }
+
+object exercises {}
